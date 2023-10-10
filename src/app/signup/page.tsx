c19c4022f70180ae1d40615aefb283b83d327127
Lesley Chang <lesley.hanna98@gmail.com>
2023-10-10 03:57:42 -0700

    basic logic

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,33 @@
 "use client";
 
+import type { ChangeEventHandler } from "react";
+import { useState } from "react";
+
 import { useStepper } from "@/hooks/use-stepper";
 import { SIGNUP_STEPS } from "@/modules/signup/signup.types";
 
 const SignUpPage = () => {
   const { step, back, next, isFirst, isLast } = useStepper(SIGNUP_STEPS);
+  const [values, setValues] = useState(
+    Object.fromEntries(SIGNUP_STEPS.map(({ id }) => [id, ""]))
+  );
 
-  const handleBackClick = () => back();
-  const handleNextClick = () => next();
+  const handleBackClick = () => {
+    back();
+  };
+  const handleNextClick = () => {
+    next();
+  };
+
+  const isNextDisabled = (): boolean => {
+    return !values[step.id];
+  };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setValues((prev) => ({ ...prev, [step.id]: event.target.value }));
+  };
+
+  const handleSubmit = () => {};
 
   return (
     <div className="flex flex-col gap-8">
@@ -24,13 +44,19 @@ const SignUpPage = () => {
       <div>
         <label className="flex flex-col gap-2">
           {step.label}
-          <input type={step.type} name={step.id} />
+          <input
+            type={step.type}
+            name={step.id}
+            value={values[step.id]}
+            onChange={handleChange}
+          />
         </label>
       </div>
       <div className="flex justify-end">
         <button
-          className="text-center font-semibold border px-6 py-2 rounded"
-          onClick={handleNextClick}
+          className="text-center font-semibold border px-6 py-2 rounded disabled:opacity-60"
+          disabled={isNextDisabled()}
+          onClick={isLast() ? handleSubmit : handleNextClick}
         >
           {isLast() ? "Submit" : "Next"}
         </button>
