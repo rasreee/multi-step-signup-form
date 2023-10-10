afd0cbfebed9aa76c9e0c00df61206d2d9f9c264
Lesley Chang <lesley.hanna98@gmail.com>
2023-10-10 11:21:58 -0700

    improve ux for signup success

--- src/app/signup/success/page.tsx
+++ src/app/signup/success/page.tsx
@@ -1,6 +1,17 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const SignUpSuccessPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/");
+    }, 2000);
+  }, []);
+
   return <div className="flex flex-col gap-8">Successfully signed up!</div>;
 };
 
